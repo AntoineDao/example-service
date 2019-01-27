4fb3b6bd0d9ee756e76c157e5aea7c73410be649
AntoineDao <antoinedao1@gmail.com>
2019-01-27 23:01:54 +0000

    fix(semantic release): for real version fix this time...

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t $TRAVIS_REPO_SLUG:$NEW_VERSION 
-docker push $TRAVIS_REPO_SLUG:$0 
+docker build . -t $TRAVIS_REPO_SLUG:$1 
+docker push $TRAVIS_REPO_SLUG:$1 
 docker push $TRAVIS_REPO_SLUG
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -9,7 +9,7 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 IFS=/ read ORG REPOSITORY <<< $TRAVIS_REPO_SLUG
-sed -i "s/{{COMMIT_TAG}}/$0/g" service.yml
+sed -i "s/{{COMMIT_TAG}}/$1/g" service.yml
 sed -i "s/{{ORG}}/$ORG/g" service.yml
 sed -i "s/{{REPOSITORY}}/$REPOSITORY/g" service.yml
 sed -i "s/{{NAMESPACE}}/$NAMESPACE/g" service.yml
