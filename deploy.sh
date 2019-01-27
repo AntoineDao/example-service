0ae43cdc2da8a5b43fc202c84a80e757bea9eeff
AntoineDao <antoinedao1@gmail.com>
2019-01-27 22:55:45 +0000

    fix(deploy): set NEW_VERSION from args

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-NEW_VERSION = $0
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t $TRAVIS_REPO_SLUG:NEW_VERSION 
-docker push $TRAVIS_REPO_SLUG:NEW_VERSION 
+docker build . -t $TRAVIS_REPO_SLUG:$NEW_VERSION 
+docker push $TRAVIS_REPO_SLUG:$0 
 docker push $TRAVIS_REPO_SLUG
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -10,7 +9,7 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 IFS=/ read ORG REPOSITORY <<< $TRAVIS_REPO_SLUG
-sed -i "s/{{COMMIT_TAG}}/NEW_VERSION/g" service.yml
+sed -i "s/{{COMMIT_TAG}}/$0/g" service.yml
 sed -i "s/{{ORG}}/$ORG/g" service.yml
 sed -i "s/{{REPOSITORY}}/$REPOSITORY/g" service.yml
 sed -i "s/{{NAMESPACE}}/$NAMESPACE/g" service.yml
