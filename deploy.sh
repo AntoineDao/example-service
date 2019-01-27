256a59b7f752438df6ea29d6069b1efba33ed693
AntoineDao <antoinedao1@gmail.com>
2019-01-27 22:38:59 +0000

    ci(semantic release): add semantic release

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+NEW_VERSION = $0
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t $TRAVIS_REPO_SLUG:$COMMIT_TAG 
-docker push $TRAVIS_REPO_SLUG:$COMMIT_TAG 
+docker build . -t $TRAVIS_REPO_SLUG:NEW_VERSION 
+docker push $TRAVIS_REPO_SLUG:NEW_VERSION 
 docker push $TRAVIS_REPO_SLUG
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -9,7 +10,7 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 IFS=/ read ORG REPOSITORY <<< $TRAVIS_REPO_SLUG
-sed -i "s/{{COMMIT_TAG}}/$COMMIT_TAG/g" service.yml
+sed -i "s/{{COMMIT_TAG}}/NEW_VERSION/g" service.yml
 sed -i "s/{{ORG}}/$ORG/g" service.yml
 sed -i "s/{{REPOSITORY}}/$REPOSITORY/g" service.yml
 sed -i "s/{{NAMESPACE}}/$NAMESPACE/g" service.yml
