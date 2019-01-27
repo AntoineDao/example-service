1c5af912c0c0560c4cfc1b030ce87b4e076a139f
AntoineDao <antoinedao1@gmail.com>
2019-01-27 11:44:10 +0000

    fix(deploy): use $COMMIT_TAG and learn how to sed

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t $TRAVIS_REPO_SLUG:$COMMIT 
-docker push $TRAVIS_REPO_SLUG:$COMMIT 
+docker build . -t $TRAVIS_REPO_SLUG:$COMMIT_TAG 
+docker push $TRAVIS_REPO_SLUG:$COMMIT_TAG 
 docker push $TRAVIS_REPO_SLUG:latest
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-sed -i 's/COMMIT_TAG/$COMMIT_TAG/g' service.yml
+sed -i "s/COMMIT_TAG/$COMMIT_TAG/g" service.yml
 
 kubectl --kubeconfig=kubeconfig apply -f service.yml
